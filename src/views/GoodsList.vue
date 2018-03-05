51ca9ee5ce8f47b2e9782117a998813f588515a4
LeannaLady <1351993796@qq.com>
2018-03-05 11:53:57 +0800

    产品价格区间增加

--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -1,52 +1,52 @@
 <template>
-	<div>
-		<nav-header></nav-header>
-		<nav-bread></nav-bread>
-		<div class="accessory-result-page accessory-page">
-		  <div class="container">
-		    <div class="filter-nav">
-		      <span class="sortby">Sort by:</span>
-		      <a href="javascript:void(0)" class="default cur">Default</a>
-		      <a href="javascript:void(0)" class="price">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
-		      <a href="javascript:void(0)" class="filterby stopPop">Filter by</a>
-		    </div>
-		    <div class="accessory-result">
-		      <!-- filter -->
-		      <div class="filter stopPop" id="filter">
-		        <dl class="filter-price">
-		          <dt>Price:</dt>
-		          <dd><a href="javascript:void(0)">All</a></dd>
-		          <dd>
-		            <a href="javascript:void(0)">0 - 100</a>
-		          </dd>
-		        </dl>
-		      </div>
+  <div>
+    <nav-header></nav-header>
+    <nav-bread></nav-bread>
+    <div class="accessory-result-page accessory-page">
+      <div class="container">
+        <div class="filter-nav">
+          <span class="sortby">Sort by:</span>
+          <a href="javascript:void(0)" class="default cur">Default</a>
+          <a href="javascript:void(0)" class="price">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
+          <a href="javascript:void(0)" class="filterby stopPop">Filter by</a>
+        </div>
+        <div class="accessory-result">
+          <!-- filter -->
+          <div class="filter stopPop" id="filter">
+            <dl class="filter-price">
+              <dt>Price:</dt>
+              <dd><a href="javascript:void(0)" v-bind:class="{cur : defaultCur== 'default'}" @click="defaultCur = 'default'">All</a></dd>
+              <dd v-for="(item,index) in priceList">
+                <a href="javascript:void(0)" @click ="defaultCur = index" v-bind:class="{cur : defaultCur == index }" >{{item.beginPrice |numFormat}} - {{item.endPrice |numFormat }}</a>
+              </dd>
+            </dl>
+          </div>
 
-		      <!-- search result accessories list -->
-		      <div class="accessory-list-wrap">
-		        <div class="accessory-list col-4">
-		          <ul>
-		            <li v-for="(k,v) in goodlist">
-		              <div class="pic">
-		                <a href="#"><img v-bind:src="'/static/'+k.productImg" alt=""></a>
-		              </div>
-		              <div class="main">
-		                <div class="name">{{k.productName}}</div>
-		                <div class="price">{{k.productPrice}}</div>
-		                <div class="btn-area">
-		                  <a href="javascript:;" class="btn btn--m">加入购物车</a>
-		                </div>
-		              </div>
-		            </li>
-		          </ul>
-		        </div>
-		      </div>
-		    </div>
-		  </div>
-		</div>
-		<nav-footer></nav-footer>
-	</div>
-	
+          <!-- search result accessories list -->
+          <div class="accessory-list-wrap">
+            <div class="accessory-list col-4">
+              <ul>
+                <li v-for="(k,v) in goodlist">
+                  <div class="pic">
+                    <a href="#"><img v-bind:src="'/static/'+k.productImg" alt=""></a>
+                  </div>
+                  <div class="main">
+                    <div class="name">{{k.productName}}</div>
+                    <div class="price">{{k.productPrice |numFormat }}</div>
+                    <div class="btn-area">
+                      <a href="javascript:;" class="btn btn--m">加入购物车</a>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <nav-footer></nav-footer>
+  </div>
+
 </template>
 <script>
 import './../assets/css/base.css'
@@ -56,33 +56,57 @@ import NavFooter from '@/components/NavFooter.vue'
 import NavBread from '@/components/NavBread.vue'
 import axios from 'axios'
 export default{
-	data(){
-		return {
-			goodlist:[],
+  data(){
+    return {
+      goodlist:[],
+      priceList:[
+        {
+          "beginPrice":"0",
+          "endPrice":"500"
+        },
+        {
+          "beginPrice":"500",
+          "endPrice":"1000"
+        },
+        {
+          "beginPrice":"1000",
+          "endPrice":"2000"
+        }
+      ],
+      defaultCur:'default',
 
-		}
-	},
-	components:{
-		NavHeader:NavHeader,
-		NavFooter:NavFooter,
-		NavBread:NavBread,
+    }
+  },
+  components:{
+    NavHeader:NavHeader,
+    NavFooter:NavFooter,
+    NavBread:NavBread,
 
-	},
-	mounted:function(){
-		this.getGoodList();
+  },
+  mounted:function(){
+    this.getGoodList();
 
-	},
-	methods:{
-		getGoodList:function(){
-			axios.get('/api/goods').then((res)=>{
-				var res = res.data;
-				this.goodlist = res.result;
-			}).catch((err)=>{
-				console.log('error:',err)
-			})
-		}
+  },
+  methods:{
+    getGoodList:function(){
+      axios.get('/api/goods').then((res)=>{
+        var res = res.data;
+        this.goodlist = res.result;
+      }).catch((err)=>{
+        console.log('error:',err)
+      })
+    }
 
-	}
+  },
+  filters:{
+    numFormat(value){
+      if( value % 1 == 0) {
+        return value+'.00'
+      }
+
+    }
+
+  }
 }
-	
+
 </script>
